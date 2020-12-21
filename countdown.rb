7dd6ad3c6c52210649cfb4e182cb40187a48d9bc
Michael Penn <mwpenn94@gmail.com>
2020-12-21 18:15:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,9 +7,10 @@ require 'pry'
 def countdown(x)
   #binding.pry
   while x > 0
-    if x == 0 
-      puts "HAPPY NEW YEAR!"
+    
     else
+      if x == 0 
+      puts "HAPPY NEW YEAR!"
       puts "{x} SECOND(S)!"
     x -= 1
    end
